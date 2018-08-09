12f4fb281f6bf8cd899ee81c90cf28ed71086800
dayfly <youzhengyouzheng666@gmail.com>
2018-08-09 14:17:24 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -15,22 +15,25 @@ def GetId(url):
 def download(url):
     id=GetId(url)
     crx='https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+id+'%26installsource%3Dondemand%26uc'
+    return HttpResponse('Hello from Python!')
+'''
     import urllib.request
     try:
         crx=urllib.request.urlopen(crx).read()
         return HttpResponse(crx)
     except:
         return HttpResponse('the link is weak')
+        '''
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
-'''
+
+
     if 'url' in request.GET:
         url = request.GET['url']
         download(url)
 
     return render(request, 'index.html')
-'''
+
 
 def db(request):
 
