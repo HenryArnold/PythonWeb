3a43a704fbef0411eded0f2217c65fe363a5fe2e
dayfly <youzhengyouzheng666@gmail.com>
2018-08-09 19:06:24 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -41,7 +41,7 @@ def index(request):
         url = request.GET['url']
         ids, name=GetId(url)
         crx='https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+ids+'%26installsource%3Dondemand%26uc'
-        return HttpResponse(crx)
+
         import urllib.request
         try:
             crx=urllib.request.urlopen(crx).read()
