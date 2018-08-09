b438e7ff9deafb9259c1046b4bff82193d1e684b
dayfly <youzhengyouzheng666@gmail.com>
2018-08-09 10:31:25 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
-# Create your views here.
-def index(request):
-#    return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
-
 def GetId(url):
     id=''
     for i in url:
@@ -18,13 +13,20 @@ def GetId(url):
     return id
 
 def download(url):
-    url = request.GET['url']
-    download(url)
     id=GetId(url)
     crx='https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+id+'%26installsource%3Dondemand%26uc'
     import urllib2
     crx=urllib2.urlopen(crx)
     return HttpResponse(crx)
+
+# Create your views here.
+def index(request):
+#    return HttpResponse('Hello from Python!')
+    if url in request:
+        url = request.GET['url']
+        download(url)
+    return render(request, 'index.html')
+
 
 def db(request):
 
