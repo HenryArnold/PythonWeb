d3d2bf40f24fe914127dc615f1827d243c87f45d
dayfly <youzhengyouzheng666@gmail.com>
2018-08-09 09:21:12 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,18 @@ def index(request):
 #    return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def GetId(url):
+    id=''
+    for i in url:
+        if i=='/':
+            id=''
+        else:
+            id=id+i
+    return id
+
+def download(url):
+    id=GetId(url)
+    crx='https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+id+'%26installsource%3Dondemand%26uc'
 
 def db(request):
 
