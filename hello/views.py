1ce2ce39ea0f3e598d0872372e63752463c2245e
dayfly <youzhengyouzheng666@gmail.com>
2018-08-11 14:06:23 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -61,30 +61,15 @@ def index(request):
     chunk_size=512
     context = {}
     context['error'] =''
-    if 'url' in request.GET:
-        url = request.GET['url']
-        ids, the_file_name=GetId(url)
-        the_file_name+=".crx"
-        crx='https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+ids+'%26installsource%3Dondemand%26uc'
-
-        '''
-        def file_iterator(file_name, chunk_size=512):
-            with open(file_name) as f:
-                while True:
-                    c = f.read(chunk_size)
-                    if c:
-                        yield c
-                    else:
-                        break
-                response = StreamingHttpResponse(file_iterator(the_file_name))
-                response['Content-Type'] = 'application/octet-stream'
-                response['Content-Disposition'] = 'attachment;filename="{0}"'.format(the_file_name)
-                return response
-        '''
-
-        import urllib.request
 
         try:
+            if 'url' in request.GET:
+                url = request.GET['url']
+                ids, the_file_name=GetId(url)
+                the_file_name+=".crx"
+                crx='https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+ids+'%26installsource%3Dondemand%26uc'
+
+            import urllib.request
             crx=urllib.request.urlopen(crx).read()
 
             '''
