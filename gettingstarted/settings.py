1bdcc9952fb6d092a40caab7425d1cba7d45e0dc
dayfly <youzhengyouzheng666@gmail.com>
2018-09-01 19:55:17 +0800

    test

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
 import django_heroku
-
+#from poem import config
+#from poem import data, model
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- poem/views.py
+++ poem/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from poem import data, model
+from poem.config import *
 # Create your views here.
 def getPoem(head):
+    trainData=data.POEMS(trainPoems)
+    xiaocai=model.MODEL(trainData)
+    poem11=xiaocai.testHead(head).replace('\n','')
+    poem22=xiaocai.testHead(head).replace('\n','')
+    poem33=xiaocai.testHead(head).replace('\n','')
     poem1=[]
     poem2=[]
     poem3=[]
     poems=[]
-    poem1.append('小家未得美声深，')
-    poem1.append('菜土裁瑶改浅深。')
-    poem1.append('威自往来全寸地，')
-    poem1.append('武平遗迹莫孤舟。')
-    poem2.append('小家未得美声深，')
-    poem2.append('菜土裁瑶改浅深。')
-    poem2.append('威自往来全寸地，')
-    poem2.append('武平遗迹莫孤舟。')
-    poem3.append('小家未得美声深，')
-    poem3.append('菜土裁瑶改浅深。')
-    poem3.append('威自往来全寸地，')
-    poem3.append('武平遗迹莫孤舟。')
+    def dealPoem(poem11, poem1):
+        str=''
+        for i in poem11:
+            str+=i
+            if i=='，':
+                poem1.append(str)
+                str=''
+            elif i=='。':
+                poem1.append(str)
+                str=''
+        return poem1
+    poem1=dealPoem(poem11, poem1)
+    poem2=dealPoem(poem22, poem2)
+    poem3=dealPoem(poem33, poem3)
     poems.append(poem1)
     poems.append(poem2)
     poems.append(poem3)
     return poems
+
 def poem(request):
     context={}
     context['poems']=[]
+    context['error']=''
     try:
         if 'head' in request.GET:
             head=request.GET['head']
@@ -32,6 +44,6 @@ def poem(request):
             return render(request, 'poem.html', context)
         else:
             return render(request, 'poem.html', context)
-    except:
-            context['error']='error'
+    except Exception as e:
+            context['error']=e
             return render(request, 'poem.html', context)
